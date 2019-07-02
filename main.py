57bf64f057485192cfa959e78dd376f72731ee8f
Nikhil Mundra <nikhilmundra29@gmail.com>
2019-07-02 18:37:00 +0530

    directory str

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/admin")
 def transport_admin():
-    return render_template("admin.html")
+    return render_template('transport_admin.html')
     
 if __name__ == "__main__":
     app.run(debug=True)
