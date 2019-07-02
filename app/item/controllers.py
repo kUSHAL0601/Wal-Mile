e26b98e17b49f9a4028cec014e345a50116a6714
kUSHAL <majmundarkushal9@gmail.com>
2019-07-02 21:40:28 +0530

    Item model added

--- app/item/controllers.py
+++ app/item/controllers.py
@@ -5,12 +5,15 @@ from .models import Item
 
 mod_item = Blueprint('item', __name__)
 
-@mod_item.route('/item', methods=['POST'])
+@mod_item.route('/itemAdd', methods=['POST'])
 def create_item():
     try:
         item_id = request.form['item_id']
+        height = request.form['height']
+        width = request.form['width']
+        fragile = request.form['fragile']
+        status = request.form['status']
         vehicle_no = request.form['vehicle_no']
-
     except KeyError as e:
         return jsonify(success=False, message="%s not sent in the request" % e.args), 400
 
--- app/item/models.py
+++ app/item/models.py
@@ -4,8 +4,16 @@ from app import db
 class Item(db.Model):
     __tablename__ = 'items'
     item_id = db.Column(db.Integer, primary_key=True)
+    height = db.Column(db.String(255))
+    width = db.Column(db.String(255))
+    fragile = db.Column(db.String(255))
+    status = db.Column(db.String(255))
     vehicle_no = db.Column(db.String(255))
 
-    def __init__(self, item_id, vehicle_no):
-        self.item_id = item_id
+
+    def __init__(self, height, width, fragile, status, vehicle_no):
+        self.height = height
+        self.width = width
+        self.fragile = fragile
+        self.status = status
         self.vehicle_no = vehicle_no
