6c42c0b0f2974fedb5eac6c319ddc36ead2d67a6
quantum109678 <akkashyap8991@gmail.com>
2019-07-03 20:51:47 +0530

    Client Location Addition done

---
+++ app/utils/client_loc/controllers.py
@@ -0,0 +1,29 @@
+from flask import *
+from sqlalchemy.exc import IntegrityError
+from app import *
+from .models import ClientLocation
+
+mod_clientlocation = Blueprint('clientlocation', __name__)
+
+@mod_clientlocation.route('/addAddress', methods=['GET'])
+def add_addr():
+    return render_template('add_addr.html')
+
+@mod_clientlocation.route('/clientlocation', methods=['POST'])
+def create_clientlocation():
+    try:
+        client_id = request.form['client_id']
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        location = request.form['location']
+    except KeyError as e:
+        return jsonify(success=False, message="%s not sent in the request" % e.args), 400
+
+    cl = ClientLocation(client_id, latitude, longitude, location)
+    db.session.add(cl)
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        return jsonify(success=False, message="This address already exists"), 400
+
+    return jsonify(success=True)
---
+++ app/utils/client_loc/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class ClientLocation(db.Model):
+    __tablename__ = 'clientlocation'
+    client_id = db.Column(db.Integer,primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    location = db.Column(db.String(255))
+    
+
+
+    def __init__(self, client_id, latitude, longitude, location):
+        self.client_id = client_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.location = location
+    
