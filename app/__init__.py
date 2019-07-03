ccbd7edbefec91000d6bfb19525a58e77c6ef25c
quantum109678 <akkashyap8991@gmail.com>
2019-07-03 22:30:14 +0530

    Partial Integration

--- app/__init__.py
+++ app/__init__.py
@@ -42,11 +42,13 @@ def home_page():
 from app.utils.vehicles.controllers import mod_vehicle
 from app.utils.item.controllers import mod_item
 from app.utils.user.controllers import mod_user
+from app.utils.client_loc.controllers import mod_clientlocation
 
 
 # Register blueprint(s)
 app.register_blueprint(mod_vehicle)
 app.register_blueprint(mod_item)
+app.register_blueprint(mod_clientlocation)
 #app.register_blueprint(mod_todo)
 
 # Build the database:
